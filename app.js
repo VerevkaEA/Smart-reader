21d46e278258304459b33eeb20f82ca85a96f35f
Evdokia Verevka <efrosinyaverevka@gmail.com>
2026-09-13 22:57:01 +0300

    Library Screen, Using IndexedDB

--- app.js
+++ app.js
@@ -22,10 +22,14 @@ const modalTranslation = document.getElementById("modal-translation");
 const modalDefinition = document.getElementById("modal-definition");
 const playAudioBtn = document.getElementById("play-audio-btn");
 const addToVocabBtn = document.getElementById("add-to-vocab-btn");
+const booksGrid = document.getElementById("books-grid");
+
+renderLibrary();
 
 backBtn.addEventListener("click", () => {
   readerScreen.classList.add("hidden");
   libraryScreen.classList.remove("hidden");
+  renderLibrary();
 });
 
 testBookCard.addEventListener("click", () => {
@@ -44,16 +48,39 @@ fileInput.addEventListener("change", (event) => {
     console.log("Файл успешно прочитан в массив байтов!");
 
     pdfjsLib.getDocument(typedarray).promise.then((pdf) => {
+      const books = JSON.parse(localStorage.getItem("books")) || [];
+      const existingBook = books.find((book) => book.title === file.name);
+
       console.log("Книга открыта, всего страниц:", pdf.numPages);
 
       bookTitle.textContent = file.name;
       readerScreen.classList.remove("hidden");
       libraryScreen.classList.add("hidden");
 
-      currentPdf = pdf;
-      totalPages = pdf.numPages;
-      currentPage = 1;
+      if (existingBook) {
+        currentPdf = pdf;
+        totalPages = pdf.numPages;
+        currentPage = existingBook.currentPage;
+      }
 
+      if (!existingBook) {
+        currentPdf = pdf;
+        totalPages = pdf.numPages;
+        currentPage = 1;
+
+        const newColor = getRandomColor();
+        const newBook = {
+          title: file.name,
+          currentPage: 1,
+          totalPages: pdf.numPages,
+          coverColor: newColor,
+        };
+
+        books.push(newBook);
+        localStorage.setItem("books", JSON.stringify(books));
+      }
+
+      idbKeyval.set(file.name, file);
       renderPage(currentPage);
     });
   };
@@ -76,6 +103,7 @@ nextBtn.addEventListener("click", () => {
   if (currentPage < totalPages) {
     currentPage++;
     renderPage(currentPage);
+    updateBookProgress();
   }
 });
 
@@ -83,6 +111,7 @@ prevBtn.addEventListener("click", () => {
   if (currentPage > 1) {
     currentPage--;
     renderPage(currentPage);
+    updateBookProgress();
   }
 });
 
@@ -192,4 +221,88 @@ function getTranslation(word) {
       modalTranslation.textContent = "Translation error";
       modalDefinition.textContent = "Definition error";
     });
+}
+
+function getRandomColor() {
+  const colors = [
+    "#357abd",
+    "#0a7336",
+    "#e67e22",
+    "#ac0fea",
+    "#1abc9c",
+    "#e74c3c",
+    "#ea2070",
+    "#3f1d96",
+  ];
+  const randomindex = Math.floor(Math.random() * colors.length);
+
+  return colors[randomindex];
+}
+
+function updateBookProgress() {
+  const books = JSON.parse(localStorage.getItem("books")) || [];
+
+  const currentBook = books.find(
+    (book) => book.title === bookTitle.textContent,
+  );
+
+  if (currentBook) {
+    currentBook.currentPage = currentPage;
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+}
+
+function renderLibrary() {
+  booksGrid.innerHTML = "";
+
+  const books = JSON.parse(localStorage.getItem("books")) || [];
+
+  books.forEach((book) => {
+    const percent = Math.round((book.currentPage / book.totalPages) * 100);
+
+    const bookCard = ` <div class="book-card">
+            <div class="book-cover" style="background-color: ${book.coverColor}"></div>
+            <button class="delete-book-btn">✕</button>
+            <h3 class="book-title">${book.title}</h3>
+            <div class="progress-container">
+              <div class="progress-bar" style="width: ${percent}%; background-color: ${book.coverColor}"></div>
+            </div>
+          </div>`;
+
+    booksGrid.insertAdjacentHTML("beforeend", bookCard);
+
+    const card = booksGrid.lastElementChild;
+    const deleteBtn = card.querySelector(".delete-book-btn");
+
+    deleteBtn.addEventListener("click", async (e) => {
+      e.stopPropagation();
+      await idbKeyval.del(book.title);
+      const updatedBooks = books.filter((b) => b.title !== book.title);
+      localStorage.setItem("books", JSON.stringify(updatedBooks));
+      renderLibrary();
+    });
+
+    card.addEventListener("click", async () => {
+      const file = await idbKeyval.get(book.title);
+
+      if (!file) {
+        alert("File not found on memory");
+        return;
+      }
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const typedarray = new Uint8Array(e.target.result);
+        pdfjsLib.getDocument(typedarray).promise.then((pdf) => {
+          currentPdf = pdf;
+          totalPages = pdf.numPages;
+          currentPage = book.currentPage;
+          bookTitle.textContent = book.title;
+          readerScreen.classList.remove("hidden");
+          libraryScreen.classList.add("hidden");
+          renderPage(currentPage);
+        });
+      };
+      reader.readAsArrayBuffer(file);
+    });
+  });
 }
